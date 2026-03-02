05ac9f553b1b9f8bcd0d9d44a3c0524cebf81c2a
Davi-silva <davisilvasanttos2026@gmail.com>
2026-03-02 11:56:34 -0300

    uptade
     nessa aula fizemos o menu de pedido na hamburgueria com os nomes dos lanches

---
+++ pensamento computacional Davi silva/projeto_app_davi  s.py
@@ -0,0 +1,69 @@
+'''
+CRUD
+
+hamburgueria
+
+Entregar o cardapio
+escolher seu produto
+pagar
+retirar no caixa
+
+
+
+'''
+
+print("===Hamburgueria===")
+print(" gestão da hamburgueria")
+print("1. entregar o cardapio")
+print("2. anotar o pedido")
+print("3.fazer seu pedido")
+print("4.anotar o pagamneto do cliente")
+print("5.embalar o produto")
+print("6.entregar o pedido para o cliente")
+
+    
+while True:    
+   
+   escolha =  input("\nEscolha uma opção")
+   
+   if escolha == '1':
+    print('oi')
+   
+    
+    # codigo para entrengar cardapio
+            
+
+
+    if escolha =='0':
+        print("saindo do sistema.até logo!")
+    break
+if escolha == 
+print("opção invalida.por favor, tente novamente.")
+
+if escolha == '3':
+    print("Fazer seu pedido")
+
+        #codigo fazer seu pedido
+
+if escolha == '4':
+        print("Anotar o pagamento do cliente")
+
+        # codigo anotar o pagamento do cliente
+    
+if escolha == '5':
+        print("Embalar o produto")
+
+        # codigo embalar o produto
+
+if escolha == '6':
+        print("Entrega o pedido para o cliente")
+
+        # codigo para entrega o produto para o cliente
+
+
+if escolha ==  '1' :
+     
+     print("hamburguer x-salada")
+     print("hamburguer x-tudo")
+     print("hamburguer vegetariano")
+     print("hambuerguer x-chedder")   
