e36029171aeda91d9a2a53198c3efc37173e55a0
Davi-silva <davisilvasanttos2026@gmail.com>
2026-03-09 11:54:14 -0300

    Add files via upload
    aprendizado na vocação  sobre os codigos do github
    
    aprendemos a codificar alguns projetos..

---
+++ simplificação_Davi_silva.py
@@ -0,0 +1,14 @@
+preços_frutas={
+
+'maça':2.5,
+'banana':1.8,
+'laranja':3.0
+
+}
+    
+fruta_desejada = 'maçã'
+
+resultado = preco_frutas.pagar(fruta_desejada,'fruta não')
+
+
+print('f""o preco da{fruta_desejada}é r${resultado}')
